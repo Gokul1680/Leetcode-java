b99644d42674285ebed890b6215daa6cfa29913a
Gokul <69070641+Gokul1680@users.noreply.github.com>
2021-12-01 13:41:22 +0530

    Create containsDuplicates_217.java

---
+++ Day3/containsDuplicates_217.java
@@ -0,0 +1,11 @@
+//Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for(int i = 1 ; i < nums.length  ; i++){
+            if(nums[i-1] == nums[i]) return true;
+        }
+        return false;
+    }
+}
